77ff14d0486385b1d469615cee5ef2a8527f8d48
martonz <tonce30@hotmail.com>
2019-10-28 11:29:39 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <ESP8266Wifi.h>
-#include <bme680.h>
 
 void setup() {
-
+  // put your setup code here, to run once:
 }
 
-void main() {
-
+void loop() {
+  // put your main code here, to run repeatedly:
 }
-
-
